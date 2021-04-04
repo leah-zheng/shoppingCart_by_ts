8124f6ad77410363143e37571f2d49bdd936e4ae
leahzheng <zan272905918@163.com>
2021-04-04 23:22:32 +0800

    feat: add_cart

---
+++ src/components/cartBoard/cartBoard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator"
+
+@Component({
+    name: "CartBoard"
+})
+export default class CartBoard extends Vue {}
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/cartTitle/carTitle.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="cart-title cover">
+      <h1>购物车</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator"
+
+@Component({
+    name: "CartTitle"
+})
+export default class CartTitle extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.cart-title {
+    display: flex;
+    width: 1226px;
+    height: 3.5rem;
+    margin: 1.6rem auto 0;
+    align-items: center;
+
+    h1 {
+        font-weight: bold;
+    }
+}
+</style>
